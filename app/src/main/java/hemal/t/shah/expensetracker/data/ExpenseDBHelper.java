7fcd91a0b9f656d1f6c2a5f477cf07184ce10830
Hemal Shah <hemal.shah1996@gmail.com>
2016-12-15 16:35:04 +0530

    Created database.

--- app/src/main/java/hemal/t/shah/expensetracker/data/ExpenseDBHelper.java
+++ app/src/main/java/hemal/t/shah/expensetracker/data/ExpenseDBHelper.java
@@ -4,8 +4,6 @@ package hemal.t.shah.expensetracker.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
-
 
 /**
  * Create, Update or Delete tables from this class.
@@ -13,7 +11,7 @@ import android.util.Log;
 public class ExpenseDBHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "ExpenseDBHelper";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "ExpenseDB.db";
 
     public ExpenseDBHelper(Context context) {
@@ -22,32 +20,97 @@ public class ExpenseDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("CREATE TABLE ")
-                .append(ExpenseContract.ExpenseEntry.TABLE_NAME)
-                .append("(")
-                .append(ExpenseContract.ExpenseEntry._ID).append(
-                " INTEGER PRIMARY KEY AUTOINCREMENT,")
-                .append(ExpenseContract.ExpenseEntry.COLUMN_ABOUT).append(" TEXT NOT NULL,")
-                .append(ExpenseContract.ExpenseEntry.COLUMN_AMOUNT).append(" REAL NOT NULL);");
 
-        db.execSQL(sb.toString());
+        /**
+         * CREATE TABLE user_details(
+         * _id INTEGER PRIMARY KEY AUTOINCREMENT,
+         * name TEXT NOT NULL,
+         * email TEXT NOT NULL,
+         * user_id TEXT UNIQUE NOT NULL);
+         */
+        String sql_create_user_table = "CREATE TABLE " + ExpenseContract.UserDetailsEntry.TABLE_NAME
+                + "(" + ExpenseContract.UserDetailsEntry._ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ExpenseContract.UserDetailsEntry.COLUMN_NAME + " TEXT NOT NULL, "
+                + ExpenseContract.UserDetailsEntry.COLUMN_EMAIL + " TEXT NOT NULL, "
+                + ExpenseContract.UserDetailsEntry.COLUMN_USER_ID + " TEXT UNIQUE NOT NULL);";
 
-        for (int i = 0; i < 10; i++) {
-            Log.i(TAG, "onCreate: values inserted! ");
-            String addDataForFirstTime = "INSERT INTO " + ExpenseContract.ExpenseEntry.TABLE_NAME +
-                    "("+ ExpenseContract.ExpenseEntry.COLUMN_ABOUT+ "," + ExpenseContract
-                    .ExpenseEntry.COLUMN_AMOUNT + ")" +
-                    " VALUES (\"hemal\"," + (i * 10) + ")";
-            db.execSQL(addDataForFirstTime);
-        }
+//        Log.i(TAG, "onCreate: user table: " + sql_create_user_table);
+        db.execSQL(sql_create_user_table);
 
 
+        /**
+         * CREATE TABLE clusters(
+         * _id INTEGER PRIMARY KEY AUTOINCREMENT,
+         * title TEXT UNIQUE NOT NULL,
+         * sum REAL,
+         * is_shared INTEGER NOT NULL,
+         * timestamp TEXT);
+         */
+        String sql_create_cluster_table = "CREATE TABLE " + ExpenseContract.ClusterEntry.TABLE_NAME
+                + "(" + ExpenseContract.ClusterEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ExpenseContract.ClusterEntry.COLUMN_TITLE + " TEXT UNIQUE NOT NULL, "
+                + ExpenseContract.ClusterEntry.COLUMN_SUM + " REAL, "
+                + ExpenseContract.ClusterEntry.COLUMN_IS_SHARED + " INTEGER NOT NULL, "
+                + ExpenseContract.ClusterEntry.COLUMN_TIMESTAMP + " TEXT);";
+
+//        Log.i(TAG, "onCreate: cluster table: " + sql_create_cluster_table);
+        db.execSQL(sql_create_cluster_table);
+
+        /**
+         * CREATE TABLE expense_table(
+         * _id INTEGER PRIMARY KEY AUTOINCREMENT,
+         * about TEXT NOT NULL, amount REAL NOT NULL,
+         * timestamp TEXT, cluster_id INTEGER,
+         * FOREIGN KEY(cluster_id) REFERENCES clusters(_id) );
+         */
+        String sql_create_expenses_table = "CREATE TABLE " + ExpenseContract.ExpenseEntry.TABLE_NAME
+                + "(" + ExpenseContract.ExpenseEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ExpenseContract.ExpenseEntry.COLUMN_ABOUT + " TEXT NOT NULL, "
+                + ExpenseContract.ExpenseEntry.COLUMN_AMOUNT + " REAL NOT NULL, "
+                + ExpenseContract.ExpenseEntry.COLUMN_TIMESTAMP + " TEXT, "
+                + ExpenseContract.ExpenseEntry.COLUMN_FOREIGN_CLUSTER_ID + " INTEGER, "
+                + " FOREIGN KEY(" + ExpenseContract.ExpenseEntry.COLUMN_FOREIGN_CLUSTER_ID + ") "
+                + "REFERENCES " + ExpenseContract.ClusterEntry.TABLE_NAME + "("
+                + ExpenseContract.ClusterEntry._ID + ") );";
+
+//        Log.i(TAG, "onCreate: expenses table: " + sql_create_expenses_table);
+        db.execSQL(sql_create_expenses_table);
+
+        /**
+         * CREATE TABLE cluster_users(
+         * _id INTEGER PRIMARY KEY AUTOINCREMENT,
+         * cluster_id INTEGER NOT NULL,
+         * user_id INTEGER NOT NULL,
+         * FOREIGN KEY(cluster_id) REFERENCES clusters(_id),
+         * FOREIGN KEY(user_id) REFERENCES user_details(_id) );
+         */
+        String sql_create_cluster_users_table =
+                "CREATE TABLE " + ExpenseContract.ClusterUserEntry.TABLE_NAME
+                        + "(" + ExpenseContract.ClusterUserEntry._ID
+                        + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                        + ExpenseContract.ClusterUserEntry.COLUMN_FOREIGN_CLUSTER_ID
+                        + " INTEGER NOT NULL, "
+                        + ExpenseContract.ClusterUserEntry.COLUMN_FOREIGN_USER_ID
+                        + " INTEGER NOT NULL, "
+                        + " FOREIGN KEY("
+                        + ExpenseContract.ClusterUserEntry.COLUMN_FOREIGN_CLUSTER_ID + ")"
+                        + " REFERENCES " + ExpenseContract.ClusterEntry.TABLE_NAME + "("
+                        + ExpenseContract.ClusterEntry._ID + "), "
+                        + " FOREIGN KEY(" + ExpenseContract.ClusterUserEntry.COLUMN_FOREIGN_USER_ID
+                        + ")"
+                        + " REFERENCES " + ExpenseContract.UserDetailsEntry.TABLE_NAME + "("
+                        + ExpenseContract.UserDetailsEntry._ID + ") );";
+
+//        Log.i(TAG, "onCreate: cluster user table: " + sql_create_cluster_users_table);
+
+
+        db.execSQL(sql_create_cluster_users_table);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + ExpenseContract.ExpenseEntry.TABLE_NAME);
+        //Update later..
         onCreate(db);
     }
 }
